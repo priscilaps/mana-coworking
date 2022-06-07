da76bbad1d68d0914764c75bcfb76c450a897be3
Priscila Pereira Sampaio <62256293+priscilaps@users.noreply.github.com>
2022-06-07 14:02:29 -0300

    Renovando o site do Mana Coworking - Aguardando feedback do cliente sobre textos e imagens que devo usar.

---
+++ scripts.js
@@ -0,0 +1,66 @@
+window.addEventListener('scroll', onScroll);
+
+function onScroll() {
+  showNavOnScroll();
+  backToTopOnScroll();
+  activateMenuAtCurrentSection(home);
+  activateMenuAtCurrentSection(services);
+  activateMenuAtCurrentSection(about);
+  activateMenuAtCurrentSection(contact);
+}
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2;
+  const sectionTop = section.offsetTop;
+  const sectionHeight = section.offsetHeight;
+
+  const sectionStartReachOrPassedTargeLine = targetLine >= sectionTop;
+  const sectionEndPassedTargetLine = sectionTop + sectionHeight <= targetLine;
+
+  const sectionId = section.getAttribute('id');
+  const currentMenu = document.querySelector(`nav a[href*=${sectionId}]`);
+
+  currentMenu.classList.remove('active');
+  if (sectionStartReachOrPassedTargeLine && !sectionEndPassedTargetLine) {
+    currentMenu.classList.add('active');
+  }
+}
+
+function showNavOnScroll() {
+  if (scrollY > 0) {
+    navigation.classList.add('scroll');
+  } else {
+    navigation.classList.remove('scroll');
+  }
+}
+
+function backToTopOnScroll() {
+  if (scrollY > 550) {
+    backToTop.classList.add('show');
+  } else {
+    backToTop.classList.remove('show');
+  }
+}
+
+function openMenu() {
+  document.querySelector('body').classList.add('menu-expanded');
+}
+
+function closeMenu() {
+  document.querySelector('body').classList.remove('menu-expanded');
+}
+
+ScrollReveal({
+  origin: 'top',
+  distance: '50px',
+}).reveal(`
+  #home,
+  #home img,
+  #home .stats,
+  #services,
+  #services .card,
+  #about,
+  #about img,
+  #contact,
+  #contact img
+`);
